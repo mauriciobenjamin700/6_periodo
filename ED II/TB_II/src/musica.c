5c4440c0b4d5d3e08f2bcd4197e1467da45ccd4a
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2023-12-06 17:05:19 -0300

    Ajustes ED II

--- ED II/TB_II/src/musica.c
+++ ED II/TB_II/src/musica.c
@@ -1,16 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct Musica
 {
     char titulo[50];
     float minutos;
+} Musica;
 
-}Musica;
 
-typedef struct Lista_musicas 
+typedef struct Lista_musicas
 {
     Musica *musica;
     Musica *proxima;
     Musica *anterior;
 
-}Lista_musicas;
+} Lista_musicas;
+
+Musica *criar_musica()
+{
+    Musica *musica;
+    musica = (Musica *)malloc(sizeof(Musica));
+
+    if (!musica)
+        musica = NULL;
+
+    return musica;
+}
+
+void preencher_musica(Musica *musica, char titulo[], float minutos)
+{
+    musica->minutos = minutos;
+    strcpy(musica->titulo,titulo);    
+}
+
+Lista_musicas *iniciar_lista()
+{
+    Lista_musicas *lista = (Lista_musicas *)malloc(sizeof(Lista_musicas));
+
+
+    if (lista)
+    {
+        lista->musica = NULL;
+        lista->anterior = NULL;
+        lista->proxima = NULL;
+    }
+    else 
+        lista = NULL;
+        
+
+    return lista;
+}
+// Falta fazer
+int adicionar_musica(Lista_musicas **raiz, Musica *musica)
+{
+    int sinal = 0;
+
+    if((*raiz)->musica == NULL)
+    {
+        (*raiz)->musica = musica;
+        sinal = 1;
+    }
+
+    return sinal;
+    
+}
