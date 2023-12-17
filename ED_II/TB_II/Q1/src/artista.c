5edfebf8dbd1710cb6ceae2fd00e03685ecefa81
LazimR <lrodrigues356@gmail.com>
2023-12-16 21:47:35 -0300

    Update artista.c

--- ED_II/TB_II/Q1/src/artista.c
+++ ED_II/TB_II/Q1/src/artista.c
@@ -15,20 +15,54 @@ typedef struct Artista
     char tipo[TIPO];
     char estilo[ESTILO];
     int num_albuns;
-    RB_album *Album;
 
 }Artista;
 
 typedef struct RB_artista
 {
-    int id;
     int cor;
+
     Artista artista;
     struct RB_artista *esquerda;
     struct RB_artista *direita;
     
 }RB_artista;
 
+Artista *criar_Artista()
+{
+    Artista *artista;
+    artista = (Artista *)malloc(sizeof(Artista));
 
+    if (!artista)
+        artista = NULL;
+
+    return artista;
+}
+
+void preencher_artista(Artista *artista,char nome[NOME], char tipo[TIPO], char estilo[ESTILO], int num_albuns)
+{
+    strcpy(artista->nome,nome);
+    strcpy(artista->tipo,tipo);
+    strcpy(artista->estilo,estilo);
+    artista->num_albuns = num_albuns;
+}
+
+RB_artista * cria_no_artista(int cor,Artista artista)
+{
+    RB_artista *no;
+    no = (RB_artista *)malloc(sizeof(RB_artista));
+
+    if(no)
+    {
+        no->cor = cor;
+        no->artista = artista;
+        no->esquerda = NULL;
+        no->direita = NULL;
+    }
+    else
+        no = NULL;
+    
+    return no;
+}
 
 
