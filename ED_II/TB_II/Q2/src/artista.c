cb64090a1704a17aa1f0eafdf317360a301bc758
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2023-12-29 18:46:11 -0300

    LOOP em ED II sem motivos

--- ED_II/TB_II/Q2/src/artista.c
+++ ED_II/TB_II/Q2/src/artista.c
@@ -39,7 +39,7 @@ Args:
 Return:
     None
 */
-void preencher_artista(Artista *artista, char nome[NOME], char tipo[TIPO], char estilo[ESTILO])
+void preencher_artista(Artista *artista, char nome[], char tipo[], char estilo[])
 {
     strcpy(artista->nome, nome);
     strcpy(artista->tipo, tipo);
@@ -220,82 +220,96 @@ void quebra_no_artista(Arv_23_artista **raiz, Artista artista)
 }
 */
 
-/*Sobe um nó para tentar inserir no pai*/
-void sobe_no_artista_pai(Arv_23_artista **raiz, Artista sobiu, Arv_23_artista *esquerda, Arv_23_artista *direita)
-{
-    //se não existe um pai (o no anterior já era a raiz)
-    if((*raiz) == NULL)
-    {
-        Arv_23_artista *pai = cria_no_artista();
+/*
+Sobe uma informação de um nó atual para o seu pai,  para tentar inserir no pai essa informação
 
-        if(pai != NULL)
+Args:
+    raiz::Arv_23_artista**: Referência do ponteiro que aponta para o nó que guarda o pai
+
+*/
+
+
+Arv_23_artista *quebra_no_artista(Arv_23_artista *raiz, Artista artista)
+{
+
+    Arv_23_artista *novo_no = cria_no_artista();
+
+
+    Artista temp[3];
+    temp[0] = raiz->info1;
+    temp[1] = raiz->info2;
+    temp[2] = artista;
+
+    ordenar_artistas(&temp[0], &temp[1], &temp[2]);
+    //garantirmos que o maior fique no novo nó e o do meio fique na info2
+    raiz->info1 = temp[0];
+    raiz->info2 = temp[1];
+    novo_no->info1 = temp[2];
+
+    novo_no->qtd_infos = 1;
+    raiz->qtd_infos = 1;
+
+    Artista subiu = raiz->info2;
+    Arv_23_artista *esquerda = raiz;
+    Arv_23_artista *direita = novo_no;
+
+    Arv_23_artista *nova_raiz = raiz->pai;
+    //se não existe um pai (o no anterior já era a raiz)
+    if((nova_raiz) == NULL)
+    {
+        Arv_23_artista *padrasto = cria_no_artista();
+
+        if(padrasto != NULL)
         {
-            pai->info1 = sobiu;
-            pai->esquerda = esquerda;
-            pai->centro = direita;
-            (*raiz) = pai;
+            padrasto->info1 = subiu;
+            padrasto->esquerda = esquerda;
+            padrasto->centro = direita;
+            padrasto->qtd_infos = 1;
+            nova_raiz = padrasto;
+            
+            esquerda->pai = nova_raiz;
+            direita->pai = nova_raiz;
         }
+        else
+            printf("\nFALHA AO ALOCAR MEMORIA");
     }
     //caso exista um pai
     else
     {
         //checar se tem espaço, se tiver inserir na posição correta
-        if((*raiz)->qtd_infos == 1)
+        if((nova_raiz)->qtd_infos == 1)
         {
-            int retorno = compara_string((*raiz)->info1.nome,sobiu.nome);
+            int retorno = compara_string((nova_raiz)->info1.nome,subiu.nome);
             
             if (retorno < 0)
             {
-                (*raiz)->info2 = sobiu;
-                (*raiz)->centro = esquerda;
-                (*raiz)->direita = direita;
-                (*raiz)->qtd_infos = 2;
+                nova_raiz->info2 = subiu;
+                nova_raiz->direita = direita;
+                nova_raiz->qtd_infos = 2;
+
+                direita->pai = nova_raiz;
             }
             else if (retorno > 0)
             {
-                (*raiz)->info2 = (*raiz)->info1;
-                (*raiz)->info1 = sobiu;
-                (*raiz)->centro = esquerda;
-                (*raiz)->direita = direita;
-                (*raiz)->qtd_infos = 2;               
+                nova_raiz->info2 = (nova_raiz)->info1;
+                nova_raiz->info1 = subiu;
+
+                nova_raiz->direita = direita;
+                nova_raiz->qtd_infos = 2;
+
+                direita->pai = nova_raiz;      
             }
         }
         //caso não tenha espaço, precisa quebrar o nó novamente e repetir o processo
         else
-        {
-            printf("\nImplementar o quebra nó aqui");
-        }
+            nova_raiz = quebra_no_artista(nova_raiz,subiu);
+        
     }
-    
+
+    return nova_raiz;
 }
 
 
-void quebra_no_artista(Arv_23_artista **raiz, Artista artista)
-{
-
-    if ((*raiz)->qtd_infos == 2)
-    {
-        Arv_23_artista *novo_no = cria_no_artista();
-
-        if (novo_no != NULL)
-        {
-            Artista temp[3];
-            temp[0] = (*raiz)->info1;
-            temp[1] = (*raiz)->info2;
-            temp[2] = artista;
-
-            ordenar_artistas(&temp[0], &temp[1], &temp[2]);
-            //garantirmos que o maior fique no novo nó e o do meio fique na info2
-            (*raiz)->info1 = temp[0];
-            (*raiz)->info2 = temp[1];
-            novo_no->info1 = temp[2];
-
-            (*raiz)->qtd_infos = 1;
-            sobe_no_artista_pai(&(*raiz)->pai, (*raiz)->info2,(*raiz),novo_no);
-        }
-    }
-
-}
 
 /*
 Insere um novo nó artista em uma arvore de artistas
@@ -327,16 +341,10 @@ int inserir_artista_arv23(Arv_23_artista **raiz, Artista artista)
     }
     else if (no_artista_eh_folha(*raiz))
     {
-        if ((*raiz)->qtd_infos == 0)
-        {
-            (*raiz)->info1 = artista;
-            (*raiz)->qtd_infos = 1;
-            inseri = 1;
-        }
-
-        else if ((*raiz)->qtd_infos == 1)
+        if ((*raiz)->qtd_infos == 1)
         {
             int retorno = compara_string((*raiz)->info1.nome, artista.nome);
+            //caso o novo valor seja maior que o valor em info1
             if (retorno < 0)
             {
                 (*raiz)->info2 = artista;
@@ -351,11 +359,12 @@ int inserir_artista_arv23(Arv_23_artista **raiz, Artista artista)
                 inseri = 1;
             }
         }
-
         // caso sejá folha e tenha 2 infos, precisamos quebrar o nó
         else if ((*raiz)->qtd_infos == 2)
         {
-            printf("\nCriar função de quebra nó");
+            *raiz = quebra_no_artista((*raiz),artista);
+            if(raiz!=NULL)
+                inseri = 1;
         }
     }
     // Caso não seja folha vamos ter que percorrer a arvore para achar uma folha pronta para ser inserida
@@ -392,266 +401,23 @@ int inserir_artista_arv23(Arv_23_artista **raiz, Artista artista)
     return inseri;
 }
 
-
-
-/*
-Busca um nó artista dentro da arvore com base no nome do artista
-
-
-Args:
-    raiz::Arv_23_artista*: Ponteiro para a raiz da arvore de artistas
-    nome_artista::char: Nome do artista que estamos buscando
-
-Return:
-    artista_buscado::Arv_23_artista*: Referência do artista buscado
-
-Arv_23_artista *busca_no_artista(Arv_23_artista *raiz, char nome_artista[NOME])
+void mostrar_no_artista(Arv_23_artista *no)
 {
-    Arv_23_artista *artista_buscado = NULL;
-
-    if (raiz != NULL)
+    if(no != NULL)
     {
-        if (compara_string(nome_artista, raiz->artista.nome) == 0)
-            artista_buscado = raiz;
-
-        else if (compara_string(nome_artista, raiz->artista.nome) < 0)
-            artista_buscado = busca_no_artista(raiz->esquerda, nome_artista);
-
-        else
-            artista_buscado = busca_no_artista(raiz->direita, nome_artista);
-    }
-
-    return artista_buscado;
-}
-
-/*
-Mostra todos os dados de um Artista na tela
-
-Args:
-    artista::Artista: Estrutura artista que terá seus dados exibidos na tela
-
-Return:
-    None
-
-
-void mostrar_artista(Artista artista)
-{
-    printf("\n--------DADOS DO ARTISTA-------\n");
-    printf("\nNOME: %s\nTIPO: %s\nESTILO: %s\nTOTAL DE ALBUNS: %d\n ", artista.nome, artista.tipo, artista.estilo, artista.num_albuns);
-
-    mostrar_tudo_RB_album((artista.albuns));
-    printf("\n------------------------------");
-}
-
-/*
-Remove um nó artista de uma arvore de artistas
-
-Args:
-    raiz::Arv_23_artista: ponteiro para a raiz da arvore
-    nome_artista::char: Nome do artista que será removido
-
-Return:
-    raiz::Arv_23_artista: Ponteiro para a nova raiz da arvore após a remoção
-
-
-Arv_23_artista *remove_no_artista(Arv_23_artista *raiz, char nome_artista[NOME])
-{
-    if (compara_string(nome_artista, raiz->artista.nome) < 0)
-    {
-        if (cor_artista(raiz->esquerda) == PRETO && cor_artista(raiz->esquerda->esquerda) == PRETO)
-            raiz = move2EsqRED_artista(raiz);
-
-        raiz->esquerda = remove_no_artista(raiz->esquerda, nome_artista);
-    }
-    else
-    {
-        Arv_23_artista *aux;
-        if (cor_artista(raiz->esquerda) == VERMELHO)
-            raiz = rotacao_direita_artista(raiz);
-
-        if (compara_string(nome_artista, raiz->artista.nome) == 0 && raiz->direita == NULL)
-        {
-            aux = raiz;
-            raiz = NULL;
-            free(aux);
-        }
-        else
-        {
-            if (cor_artista(raiz->direita) == PRETO && cor_artista(raiz->direita->esquerda) == PRETO)
-                raiz = move2DirRED_artista(raiz);
-
-            if (compara_string(nome_artista, raiz->artista.nome) == 0)
-            {
-                aux = procura_menor_artista(raiz->direita);
-                raiz->artista = aux->artista;
-                raiz->direita = remover_menor_artista(raiz->direita);
-            }
-            else
-                remove_no_artista(raiz->direita, nome_artista);
-        }
-    }
-    if (raiz != NULL)
-        raiz = balancear_RB_artista(raiz);
-
-    return raiz;
-}
-
-
-Remove um nó artista de uma arvore de artistas respeitando todas as normas da Rubro Negro
-Casos de retorno:
-    1 - Sucesso
-    0 - Falha
-
-Args:
-    raiz::Arv_23_artista: Referência do ponteiro para a raiz da arvore
-    nome_artista::char: Nome do artista que será removido
-
-Return:
-    removi::int: Sinalização baseada no resultado da operação de remoção
-
-int remove_no_artista_ARVRB(Arv_23_artista **raiz, char nome_artista[NOME])
-{
-    int removi = 0;
-
-    if (busca_no_artista(*raiz, nome_artista) != NULL)
-    {
-        Arv_23_artista *aux = *raiz;
-        *raiz = remove_no_artista(aux, nome_artista);
-
-        troca_cor_raiz_RB_artista(*raiz);
-
-        removi = 1;
-    }
-
-    return removi;
-}
-
-/*
-Cadastra um album em um artista e retorna o resultado do processo
-    1 - Sucesso
-    0 - Falha
-   -1 - Já estava cadastrado
-
-Args:
-    artista::Arv_23_artista*: Refência do nó que contem o artista que receberá um novo album
-    album::Arv_23_album*: Referência para o album que será adicionando ao artista
-
-Return:
-    cadastrei::int: Sinalização referente ao resultado da operação
-
-int cadastrar_album_artista(Arv_23_artista *artista, Arv_23_album *album)
-{
-    int cadastrei = 0;
-
-    if (artista != NULL)
-    {
-        cadastrei = insere_no_RB_album(&(artista->artista.albuns), album);
-
-        if (cadastrei == 1)
-            artista->artista.num_albuns++;
-    }
-
-    return cadastrei;
-}
-
-/*
-Busca um album dentro de um artista, podendo retornar a referência daquele album em caso de encontrar ou NULL caso falhe
-
-Args:
-    artista::Arv_23_artista*: Referência do nó que contem o artista que iremos acessar buscando o album.
-    titulo_album::char: Titulo do album que estaremos buscando.
-
-Return:
-    no_album_buscado::Arv_23_album*: Referência do album buscado
-
-Arv_23_album *buscar_album_artista(Arv_23_artista *artista, char titulo_album[TAM_TITULO])
-{
-    Arv_23_album *no_album_buscado = NULL;
-
-    if (artista != NULL)
-        no_album_buscado = buscar_no_RB_album(artista->artista.albuns, titulo_album);
-
-    return no_album_buscado;
-}
-
-/*
-Remove um nó album de uma arvore de albuns respeitando todas as normas da Rubro Negro e decrementa a quantidade de albuns do artista
-Casos de retorno:
-    1 - Sucesso
-    0 - Falha
-
-Args:
-    artista::Arv_23_artista*: Referência do artista que terá um album removido
-    titulo_album::char: Nome do artista que será removido
-
-Return:
-    removi::int: Sinalização baseada no resultado da operação de remoção
-
-int remover_album_artista(Arv_23_artista *artista, char titulo_album[TAM_TITULO])
-{
-    int removi = remove_no_album_ARVRB(&(artista->artista.albuns), titulo_album);
-    if (removi == 1)
-        artista->artista.num_albuns--;
-
-    return removi;
-}
-
-/*
-Retorna a confirmação sobre o ato de remover um nó
-    1 - Pode
-    0 - Não
-
-Args:
-    no::Arv_23_artista*: Referencia do nó artista que estamos checando a possibilidade de removelo
-
-Return:
-    confirmacao::int: Sinalização de confirmação sobre o ato de remover
-
-int pode_remover_artista(Arv_23_artista *no)
-{
-    int confirmacao = 0;
-
-    if (no->artista.num_albuns == 0)
-        confirmacao = 1;
-
-    return confirmacao;
-}
-
-/*
-Função para visualizar se o nó está na cor certa
-
-void mostrar_no_artista(Arv_23_artista *artista)
-{
-    printf("\n\n------------------------\n");
-    printf("\n\tCor: %d\n\tNome: %s", artista->cor_artista, artista->artista.nome);
-    printf("\n\n------------------------\n");
-}
-
-/*
-Função para percorrer a arvore e ver se as cores dos nós estão seguindo as normas vistas em sala de aula
-
-void mostrar_todos_nos_artista_ordenado(Arv_23_artista *artista)
-{
-    if (artista != NULL)
-    {
-        mostrar_todos_nos_artista_ordenado(artista->esquerda);
-        mostrar_no_artista(artista);
-        mostrar_todos_nos_artista_ordenado(artista->direita);
+        printf("\n\nInfo1: %s", no->info1.nome);
+        if(no->qtd_infos==2)
+            printf("\nInfo2: %s", no->info2.nome);
     }
 }
 
-/*
-Remove todos os artistas de uma arvore
-
-void remover_todos_artistas(Arv_23_artista **raiz)
+void mostrar_arv_artistas(Arv_23_artista *raiz)
 {
-    if (*raiz != NULL)
+    if(raiz != NULL)
     {
-        remover_todos_artistas(&(*raiz)->esquerda);
-        remover_todos_artistas(&(*raiz)->direita);
-
-        remover_todos_albuns(&(*raiz)->artista.albuns);
-        free(*raiz);
+        mostrar_no_artista(raiz);
+        mostrar_arv_artistas(raiz->esquerda);
+        mostrar_arv_artistas(raiz->centro);
+        mostrar_arv_artistas(raiz->direita);
     }
 }
-*/
---
+++ ED_II/TB_II/Q2/src/teste.c
@@ -0,0 +1,37 @@
+#include "artista.c"
+
+int main(void)
+{
+    Arv_23_artista *arvore_artistas = NULL;
+    Artista artista;
+    char dado[NOME];
+
+
+    int opc = 1;
+    
+    while(opc)
+    {
+        printf("\nPreencha os dados do artista:\n");
+        printf("\n\tNOME: ");
+        setbuf(stdin,NULL);
+        scanf("%[^\n]", dado);
+
+        
+        preencher_artista(&artista,dado,dado,dado);
+
+        if (inserir_artista_arv23(&arvore_artistas,artista)==1)
+            printf("\n\nINSERIDO COM SUCESSO");
+        else
+            printf("\n\nALGUMA FALHA ACONTECEU");
+
+
+        printf("\n\nARVORE DE ARTISTAS A BAIXO \n");
+        mostrar_arv_artistas(arvore_artistas);
+
+        printf("\n\nContinuar: [1][0]\n");
+        setbuf(stdin,NULL);
+        scanf("%d", &opc);
+    }
+
+    return 0;
+}
